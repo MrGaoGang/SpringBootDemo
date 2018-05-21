f7398adb315db6326b72c24c5a6f8e3027f0abdb
MrGaoGang <gaogangwork@qq.com>
2018-05-21 20:08:17 +0800

    完成Spring Boot和Mybatis的xml方式结合； 添加了alibaba的数据库连接池druid

---
+++ spring-mybatis-xml-demo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+
+import com.github.pagehelper.PageHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping(value = "/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @ResponseBody
+    @RequestMapping(value = "/add", produces = {"application/json;charset=UTF-8"})
+    public int addUser(User user) {
+        return userService.addUser(user);
+    }
+
+    @ResponseBody
+    @GetMapping("/all")
+    public Object findAllUser(
+            @RequestParam(name = "pageNum", required = false, defaultValue = "1")
+                    int pageNum,
+            @RequestParam(name = "pageSize", required = false, defaultValue = "10")
+                    int pageSize) {
+        //开始分页
+        PageHelper.startPage(pageNum, pageSize);
+        return userService.findAllUser(pageNum, pageSize);
+    }
+}
---
+++ spring-mybatis-xml-demo/src/main/java/com/example/demo/mapper/UserMapper.java
@@ -0,0 +1,22 @@
+package com.example.demo.mapper;
+
+import com.example.demo.model.User;
+
+import java.util.List;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer userId);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer userId);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+
+    //自己添加的
+    List<User> selectAllUser();
+}
